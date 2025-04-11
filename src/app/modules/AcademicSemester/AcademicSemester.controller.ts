b56754db2eb39556c86bd1221b117f6eaa1f28f2
mdmasudranainfo <mdmasude7@gmail.com>
2025-04-11 11:24:38 +0600

    code optimize done

--- src/app/modules/AcademicSemester/AcademicSemester.controller.ts
+++ src/app/modules/AcademicSemester/AcademicSemester.controller.ts
@@ -1,23 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import { AcademicSemesterService } from "./AcademicSemester.service";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import status from "http-status";
 
-const createAcademicSemester = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
+const createAcademicSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const data = req.body;
     const result = await AcademicSemesterService.createAcademicSemester(data);
-    res.status(200).json({
-      status: "success",
+
+    sendResponse(res, {
+      statusCode: status.CREATED,
+      success: true,
       message: "Academic semester created successfully",
       data: result,
     });
-  } catch (err) {
-    next(err);
   }
-};
+);
 
 export const AcademicSemesterController = {
   createAcademicSemester,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,21 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import { userService } from "./user.service";
 import { z } from "zod";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import status from "http-status";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    // validate the request body
-
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     // Create a new user with the received data
     const user = req.body;
     const newUser = await userService.createUser(user);
+
+    sendResponse(res, {
+      statusCode: status.CREATED,
+      success: true,
+      message: "User created successfully X",
+      data: newUser,
+    });
+
     res.status(201).json({ success: true, data: newUser });
-  } catch (err) {
-    next(err);
-    // res.status(500).json({ success: false, message: "User Created Failed" });
-    // console.log(err);
   }
-};
+);
 
 export const userController = {
   createUser,
